dc0a671276edcfae659faf1b899468e7286a1d91
Sean Kelly <shonkelli@gmail.com>
2019-02-04 23:01:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 # Write your code here.
 def line(loc)
+  output = ""
   if loc.empty?
-    "The line is currently empty."
+    output = "The line is currently empty."
   else
     output = "The line is currently:"
     pos = 1
