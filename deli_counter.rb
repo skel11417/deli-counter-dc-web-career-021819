71b49b4f3924d1841c8d1a506fb21808e41cfbc9
Sean Kelly <shonkelli@gmail.com>
2019-02-04 22:59:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,12 @@
 # Write your code here.
 def line(loc)
   if loc.length > 0 
-    
+    output = "The line is currently:"
+    pos = 1
+    loc.each do |name|
+      output << " #{pos}. {name}"
+      pos += 1 
+    end
 end 
 
 def take_a_number(loc, name)
