c8f07a3b968e8a4831ee2d88946e1259c98772fe
Sean Kelly <shonkelli@gmail.com>
2019-02-04 23:09:11 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def line(loc)
+  if loc.empty?
+    puts "The line is currently empty."
+  else
+    output = "The line is currently:"
+    pos = 1
+    loc.each do |name|
+      output << " #{pos}. #{name}"
+      pos += 1 
+    end
+    puts output
+  end
+end 
+
+def take_a_number(location, name)
+  location << name
+  puts "Welcome, #{name}. You are number #{location.length} in line."
+end
+
+def now_serving(location)
+  if location.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    current_customer = location.shift
+    puts "Currently serving #{current_customer}."
+  end
+end
